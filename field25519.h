b834c206570ee746eb9cf48701301c41f3ff81e1
Anuj <anujkumarsharma1808@gmail.com>
2023-09-16 09:52:25 +0530

    Finite field arithmetic

---
+++ field25519.h
@@ -0,0 +1,22 @@
+#pragma once
+
+using i64 = long long;
+
+struct i256 {
+
+        // std::vector<i64> data;
+
+        i256(i64 value);
+
+        i256 operator[](int index);
+        const i256 operator[](int index) const;
+
+        i256 operator+(const i256 &other) const;
+        i256 operator+(const i64 &other) const;
+        i256 operator-(const i64 &other) const;
+        i256 operator*(const i256 &other) const;
+        void carry();
+};
+
+i256 pow(i256 &a, i256 &e);
+i256 inverse(i256 a);
