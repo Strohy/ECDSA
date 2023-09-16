81cfec3c2822d8a8f7d87801a0ae8af63d716931
Anuj <anujkumarsharma1808@gmail.com>
2023-09-16 16:20:02 +0530

    Fix field25519.h syntax

--- field25519.cpp
+++ field25519.cpp
@@ -1,101 +1,90 @@
 #include <vector>
-#include <field25519.h>
+#include "field25519.h"
 
 
-struct i256
-{
-        std::vector<i64> data;
+
+i256::i256(i64 value) {
+        data.resize(16);
+        for(int i = 0; i < 16; ++i) {
+                data[i] = value;
+        }
+}
+
+i64& i256::operator[](int index) {
+        return data[index];
+}
+
+const i64& i256::operator[](int index) const{
+        return data[index];
+}
 
 
-        // i256(void) {
-        //         data.resize(16);
-        //         for(int i = 0; i < 16; ++i) {
-        //                 data[i] = 0;
-        //         }
-        // }
 
-        i256(i64 value = 0) {
-                data.resize(16);
-                for(int i = 0; i < 16; ++i) {
-                        data[i] = value;
-                }
+i256 i256::operator+(const i256 &other) const {
+        i256 result;
+
+        for(int i = 0; i < 16; ++i) {
+                result[i] = data[i] + other.data[i];
         }
 
-        i64 &operator[](int index) {
-                return data[index];
-        }
-        const i64 &operator[](int index) const{
-                return data[index];
-        }
+        return result;
+}
 
+
+i256 i256::operator+(const i64 &other) const {
+        i256 result;
+        i64 value = other;
+
+        for (int i = 0; i < 16; ++i) {
+                result[i] = data[i] + (value & 0xffff);
+                value >>= 16;
+        }
+        result.carry();
+        return result;
+}
+
+
+i256 i256::operator-(const i64 &other) const {
+        i256 result;
+        i64 value = other;
+
+        for(int i = 0; i < 16; ++i) {
+                result[i] = data[i] - (value & 0xffff);
+                value >>= 16;
+        }
+        result.carry();
+        return result;
+}
+
+
+i256 i256::operator*(const i256 &other) const {
+        i256 result;
+        std::vector<i64> temp(32);
+
+        for(int i = 0; i < 16; ++i)
+                for(int j = 0; j < 16; j++)
+                        temp[i+j] = data[i] * other[j];
+
+        for(int i = 0; i < 16; i++)
+                result[i] += 38 * temp[i+16];
         
+        result.carry();
+        result.carry();
+        result.carry();
 
-        i256 operator+(const i256 &other) const {
-                i256 result;
+        return result;
+}
 
-                for(int i = 0; i < 16; ++i) {
-                        result[i] = data[i] + other.data[i];
-                }
 
-                return result;
+void i256::carry() {
+        i64 c;
+        for(int i = 0; i < 16; ++i) {
+                c = data[i] >> 16;
+                data[i] -= c << 16;
+                if(i < 15)      data[i+1] += c;
+                else            data[0] += 38*c;
         }
-
-
-        i256 operator+(const i64 &other) const {
-                i256 result;
-                i64 value = other;
-
-                for (int i = 0; i < 16; ++i) {
-                        result[i] = data[i] + (value & 0xffff);
-                        value >>= 16;
-                }
-                result.carry();
-                return result;
-        }
-        
-
-        i256 operator-(const i64 &other) const {
-                i256 result;
-                i64 value = other;
-
-                for(int i = 0; i < 16; ++i) {
-                        result[i] = data[i] - (value & 0xffff);
-                        value >>= 16;
-                }
-                result.carry();
-                return result;
-        }
-
-
-        i256 operator*(const i256 &other) const {
-                i256 result;
-                std::vector<i64> temp(32);
-
-                for(int i = 0; i < 16; ++i)
-                        for(int j = 0; j < 16; j++)
-                                temp[i+j] = data[i] * other[j];
-
-                for(int i = 0; i < 16; i++)
-                        result[i] += 38 * temp[i+16];
-               
-                result.carry();
-                result.carry();
-                result.carry();
-
-                return result;
-        }
-        
-
-        void carry() {
-                i64 c;
-                for(int i = 0; i < 16; ++i) {
-                        c = data[i] >> 16;
-                        data[i] -= c << 16;
-                        if(i < 15)      data[i+1] += c;
-                        else            data[0] += 38*c;
-                }
-        }
-};
+}
 
 
 
@@ -123,19 +112,3 @@ i256 inverse(i256 a) {
 
         return pow(a, q);
 }
-
-
-
-
-// Delete main
-
-// int main() {
-//         i64 a = 5;
-//         i256 V(12);
-//         i256 q;
-//         for(int i = 0; i < 16; ++i) q[i] = i+1;
-//         // q[15] &= 0x0fff;
-//         q = q+2;
-
-//         for(int i = 0; i < 16; ++i) std::cout << q[i] << " ";
-// }
--- field25519.h
+++ field25519.h
@@ -1,15 +1,15 @@
 #pragma once
-
+#include<vector>
 using i64 = long long;
 
 struct i256 {
 
-        // std::vector<i64> data;
+        std::vector<i64> data;
 
-        i256(i64 value);
+        i256(i64 value = 0);
 
-        i256 operator[](int index);
-        const i256 operator[](int index) const;
+        i64& operator[](int index);
+        const i64& operator[](int index) const;
 
         i256 operator+(const i256 &other) const;
         i256 operator+(const i64 &other) const;
